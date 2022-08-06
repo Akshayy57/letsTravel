3af60705df9f02a0e16687b6e0015ef868b43df1
Akshay <yadavakshay5720@gmail.com>
2022-08-06 12:23:15 +0530

    Updates

---
+++ precache-manifest.877e13a7971a8d4a673397b41714d3a5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac85e0999c80565bd41b7cb38eecb870",
+    "url": "/letsTravel/index.html"
+  },
+  {
+    "revision": "f618ebf8b5acbb964ad0",
+    "url": "/letsTravel/static/css/2.3f2c4ce9.chunk.css"
+  },
+  {
+    "revision": "7db753e8e5c87515a2d4",
+    "url": "/letsTravel/static/css/main.c06f0a9f.chunk.css"
+  },
+  {
+    "revision": "f618ebf8b5acbb964ad0",
+    "url": "/letsTravel/static/js/2.fb04481a.chunk.js"
+  },
+  {
+    "revision": "1db7e56908ae5431f44bfad8360c69d6",
+    "url": "/letsTravel/static/js/2.fb04481a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db753e8e5c87515a2d4",
+    "url": "/letsTravel/static/js/main.94d67c3c.chunk.js"
+  },
+  {
+    "revision": "4eec981a3a2f8452150b",
+    "url": "/letsTravel/static/js/runtime-main.112ab2bf.js"
+  },
+  {
+    "revision": "14bd02dafd80dde673a6e8fa09e48226",
+    "url": "/letsTravel/static/media/arrow-left.14bd02da.svg"
+  },
+  {
+    "revision": "f95a1b0cf4a16758beccbb320920da43",
+    "url": "/letsTravel/static/media/arrow-right.f95a1b0c.svg"
+  },
+  {
+    "revision": "5930a183f9be0c913e9d25b1318416f9",
+    "url": "/letsTravel/static/media/contact1.5930a183.png"
+  },
+  {
+    "revision": "7d2fd20e2f55c60a0d6e075de3b177fe",
+    "url": "/letsTravel/static/media/desk-bell.7d2fd20e.svg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/letsTravel/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "4ba68518ed8ba92be0b8946a63fe6276",
+    "url": "/letsTravel/static/media/hero-banner.4ba68518.jpg"
+  },
+  {
+    "revision": "3e1de09c5d7d0b83f9e032c5c2191934",
+    "url": "/letsTravel/static/media/hotel1.3e1de09c.jpg"
+  },
+  {
+    "revision": "b902d4ee7f1fccbac23eeb94bce3db7a",
+    "url": "/letsTravel/static/media/hotel2.b902d4ee.jpg"
+  },
+  {
+    "revision": "d8c0f18415a187d16b75dafc2d63e1d5",
+    "url": "/letsTravel/static/media/hotel3.d8c0f184.jpg"
+  },
+  {
+    "revision": "b3b7a0210038e9a9f2fcb54f17d0f1c1",
+    "url": "/letsTravel/static/media/instagram.b3b7a021.svg"
+  },
+  {
+    "revision": "83ea9310a9781963a3eb9bd4d199ca7a",
+    "url": "/letsTravel/static/media/quote-right.83ea9310.svg"
+  },
+  {
+    "revision": "170786df6dc4088568f47d629e9cf756",
+    "url": "/letsTravel/static/media/single-bed.170786df.svg"
+  },
+  {
+    "revision": "46b4ee4396c2c9c3afb8bcd25f881f11",
+    "url": "/letsTravel/static/media/times.46b4ee43.svg"
+  },
+  {
+    "revision": "9834526afbb7fe1c0d96826410d99bd8",
+    "url": "/letsTravel/static/media/towels.9834526a.svg"
+  },
+  {
+    "revision": "6de37c77002e9013e5fb0659418ff8f4",
+    "url": "/letsTravel/static/media/twitter.6de37c77.svg"
+  },
+  {
+    "revision": "883dbd5432f1d6ad2864869f6aee72c5",
+    "url": "/letsTravel/static/media/wifi.883dbd54.svg"
+  }
+]);
